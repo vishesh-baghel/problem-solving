773e6d289d0f7c9d8f9cb8938d5dd96e9fa6c16d
vishesh <visheshbaghel99@gmail.com>
2024-09-13 22:23:26 +0530

    solved 236A

---
+++ Codeforces - Codeforces Round 146 Div 2/A. Boy or Girl/Main.java
@@ -0,0 +1,46 @@
+//  A. Boy or Girl
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    private static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+    static List<String> readAllLines() throws IOException {
+        List<String> lines = new ArrayList<>();
+        String line;
+        while ((line = input.readLine()) != null && !line.trim().isEmpty()) {
+            lines.add(line);
+        }
+        return lines;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Locale.setDefault(Locale.US);
+        List<String> inputLines = Main.readAllLines();
+
+        solution(inputLines);
+
+    }
+
+    public static void solution(List<String> inputLines) {
+        for (String line : inputLines) {
+            printSolution(line.trim());
+        }
+    }
+
+    public static void printSolution(String input) {
+        String[] letters = input.split("");
+        Set<String> set = new HashSet<>();
+        for (String letter : letters) {
+            set.add(letter);
+        }
+
+        if (set.size() % 2 == 0) {
+            System.out.println("CHAT WITH HER!");
+        } else {
+            System.out.println("IGNORE HIM!");
+        }
+    }
+}
