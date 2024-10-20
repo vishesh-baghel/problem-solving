7fa59ee94168ad08116e4ccb69b352c72a30ad3e
vishesh <visheshbaghel99@gmail.com>
2024-10-20 22:19:08 +0530

    solved 1374C

--- Codeforces - Codeforces Round 653 Div 3/C. Move Brackets/Main.java
+++ Codeforces - Codeforces Round 653 Div 3/C. Move Brackets/Main.java
@@ -3,6 +3,7 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+import java.util.stream.Collectors;
 
 public class Main {
     private static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -25,12 +26,28 @@ public class Main {
     }
 
     public static void solution(List<String> inputLines) {
-        for (String line : inputLines) {
-            printSolution(line.trim());
+        int inputLength = Integer.parseInt(inputLines.get(0)) * 2;
+        for (int i = 1; i < inputLength; i = i + 2) {
+            printSolution(inputLines.get(i), inputLines.get(i + 1));
         }
     }
 
-    public static void printSolution(String input) {
-        System.out.println(input);
+    public static void printSolution(String length, String bracketsString) {
+        List<String> brackets = Arrays.stream(bracketsString.split("")).collect(Collectors.toList());
+        int moves = 0;
+        int balance = 0;
+        for (int i = 0; i < brackets.size(); i++) {
+            if (brackets.get(i).equals("(")) {
+                balance++;
+            } else {
+                balance--;
+                if (balance < 0) {
+                    balance = 0;
+                    moves++;
+                }
+            }
+        }
+
+        System.out.println(moves);
     }
 }
