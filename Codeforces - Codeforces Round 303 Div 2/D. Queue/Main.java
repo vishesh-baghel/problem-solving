c95139996d2100661adccf47a7be7b7e371a7bf8
vishesh <visheshbaghel99@gmail.com>
2024-11-09 20:05:11 +0530

    solved 545D

--- Codeforces - Codeforces Round 303 Div 2/D. Queue/Main.java
+++ Codeforces - Codeforces Round 303 Div 2/D. Queue/Main.java
@@ -3,6 +3,7 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+import java.util.stream.Collectors;
 
 public class Main {
     private static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -25,12 +26,44 @@ public class Main {
     }
 
     public static void solution(List<String> inputLines) {
-        for (String line : inputLines) {
-            printSolution(line.trim());
+        for (int i = 0; i < inputLines.size() - 1; i++) {
+            printSolution(inputLines.get(i), inputLines.get(i + 1));
         }
     }
 
-    public static void printSolution(String input) {
-        System.out.println(input);
+    public static void printSolution(String input1, String input2) {
+        List<Integer> servingTimes = Arrays.stream(input2.split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+
+        Collections.sort(servingTimes);
+        int happyPersons = 0;
+        int sum = 0;
+
+        for (int i = 0; i < servingTimes.size(); i++) {
+            if (sum <= servingTimes.get(i)) {
+                happyPersons++;
+                sum += servingTimes.get(i);
+            }
+        }
+
+        System.out.println(happyPersons);
     }
+
+    /*
+    constraints
+    1. the disappointment is the total sum of all the persons serving time before a person in observation
+
+    objective: find the maximum number of not disappointed persons
+
+    observations:
+    1. we have to always sort the list to get the desired placement of persons
+    2. first person is always not disappointed
+
+    logic:
+    1. sort the list of persons in ascending order
+    2. if the sum of serving time for a person is less than or equal to the previous persons, then
+        increment the count of not disappointed persons
+
+    13 2 5 55 21 34 1 8 1 3
+
+    */
 }
